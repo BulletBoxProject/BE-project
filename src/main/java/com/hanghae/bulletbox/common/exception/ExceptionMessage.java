25171b2fc7ec5fb9305893b966a4b5a9be7f114c
jaekyeong1 <kingjk0157@naver.com>
2023-01-06 19:32:29 +0900

    feat: 토큰 예외처리 추가

--- src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
+++ src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
@@ -4,7 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionMessage {
-    EXCEPTION_MESSAGE_SAMPLE(401,"예외 내용을 적어주세요");
+    EXCEPTION_MESSAGE_SAMPLE(401,"예외 내용을 적어주세요"),
+
+    /*Jwt, Security */
+    TOKEN_NOT_FOUND_MSG(401,"토큰이 존재하지 않습니다."),
+
+    INVALID_TOKEN_MSG(401,"토큰이 유효하지 않습니다.");
+
     private final int statusCode;
     private final String msg;
 
