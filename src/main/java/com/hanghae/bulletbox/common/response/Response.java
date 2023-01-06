20f01eb6ab281189e27fc1c2cc872d69f3ea3e18
jaekyeong1 <kingjk0157@naver.com>
2023-01-06 14:50:18 +0900

    feat: Response 설정

--- src/main/java/com/hanghae/bulletbox/common/response/Response.java
+++ src/main/java/com/hanghae/bulletbox/common/response/Response.java
@@ -1,4 +1,30 @@
 package com.hanghae.bulletbox.common.response;
 
-public class Response {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+
+    private int httpStatusCode;
+    private String msg;
+    private T data;
+
+    @Builder
+    public Response(int httpStatusCode, String msg, T data) {
+        this.httpStatusCode = httpStatusCode;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static <T> Response<T> success(int httpStatusCode, String msg, T data) {
+        return new Response<>(httpStatusCode, msg, data);
+    }
+
+    public static <T> Response<T> fail(int httpStatusCode, String errorMessage) {
+        return new Response<>(httpStatusCode, errorMessage, null);
+    }
 }
