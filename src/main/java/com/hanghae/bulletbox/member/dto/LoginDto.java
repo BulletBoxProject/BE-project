fa0a165be08610552e912377c18848da1d54b12a
jaekyeong1 <kingjk0157@naver.com>
2023-01-06 22:25:47 +0900

    feat: 로그인 dto 추가

---
+++ src/main/java/com/hanghae/bulletbox/member/dto/LoginDto.java
@@ -0,0 +1,20 @@
+package com.hanghae.bulletbox.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginDto {
+
+    private String email;
+    private String password;
+
+    @Builder
+    public LoginDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
