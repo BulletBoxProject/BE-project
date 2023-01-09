42ada81d332747649a7bb92415a473eb4cb6eb7f
jaekyeong1 <kingjk0157@naver.com>
2023-01-09 16:26:04 +0900

    refactor: import 개행

--- src/main/java/com/hanghae/bulletbox/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hanghae/bulletbox/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hanghae.bulletbox.common.exception;
 
 import com.hanghae.bulletbox.common.response.Response;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
