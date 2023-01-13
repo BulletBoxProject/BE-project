009ccf591986d2f04112aa9970d161055b11abea
hangj97 <gksruwls@naver.com>
2023-01-13 15:09:11 +0900

    refactor: Builder 패턴 수정

--- src/main/java/com/hanghae/bulletbox/todo/dto/TodoDto.java
+++ src/main/java/com/hanghae/bulletbox/todo/dto/TodoDto.java
@@ -3,6 +3,7 @@ package com.hanghae.bulletbox.todo.dto;
 import com.hanghae.bulletbox.todo.TodoBullet;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,17 +11,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TodoDto {
     private Long todoId;
+
     private Long memberId;
+
     private Long categoryId;
+
     private String categoryColor;
+
     private TodoBullet todoBullet;
+
     private String todoContent;
+
     private Long todoYear;
+
     private Long todoMonth;
+
     private Long todoDay;
+
     private String dayOfTheWeek;
+
     private String time;
 
+    @Builder(access = AccessLevel.PRIVATE)
     private TodoDto(Long todoId, Long memberId, Long categoryId, String categoryColor, TodoBullet todoBullet, String todoContent, Long todoYear, Long todoMonth, Long todoDay, String dayOfTheWeek, String time) {
         this.todoId = todoId;
         this.memberId = memberId;
@@ -35,10 +47,6 @@ public class TodoDto {
         this.time = time;
     }
 
-    public static TodoDto toTodoDto(Long todoId, Long memberId, Long categoryId, String categoryColor, TodoBullet todoBullet, String todoContent, Long todoYear, Long todoMonth, Long todoDay, String dayOfTheWeek, String time) {
-        return new TodoDto(todoId, memberId, categoryId, categoryColor, todoBullet, todoContent, todoYear, todoMonth, todoDay, dayOfTheWeek, time);
-    }
-
     public static TodoDto toTodoDto(Long memberId, Long todoYear, Long todoMonth, Long todoDay) {
         return TodoDto.builder()
                 .memberId(memberId)
@@ -48,84 +56,13 @@ public class TodoDto {
                 .build();
     }
 
-    // private 빌더 패턴 적용
-    private static TodoDtoBuilder builder() {
-        return new TodoDtoBuilder();
-    }
-
-    public static class TodoDtoBuilder {
-        private Long todoId;
-        private Long memberId;
-        private Long categoryId;
-        private String categoryColor;
-        private TodoBullet todoBullet;
-        private String todoContent;
-        private Long todoYear;
-        private Long todoMonth;
-        private Long todoDay;
-        private String dayOfTheWeek;
-        private String time;
-
-        TodoDtoBuilder() {
-        }
-
-        private TodoDtoBuilder todoId(Long todoId) {
-            this.todoId = todoId;
-            return this;
-        }
-
-        private TodoDtoBuilder memberId(Long memberId) {
-            this.memberId = memberId;
-            return this;
-        }
-
-        private TodoDtoBuilder categoryId(Long categoryId) {
-            this.categoryId = categoryId;
-            return this;
-        }
-
-        private TodoDtoBuilder categoryColor(String categoryColor) {
-            this.categoryColor = categoryColor;
-            return this;
-        }
-
-        private TodoDtoBuilder todoBullet(TodoBullet todoBullet) {
-            this.todoBullet = todoBullet;
-            return this;
-        }
-
-        private TodoDtoBuilder todoContent(String todoContent) {
-            this.todoContent = todoContent;
-            return this;
-        }
-
-        private TodoDtoBuilder todoYear(Long todoYear) {
-            this.todoYear = todoYear;
-            return this;
-        }
-
-        private TodoDtoBuilder todoMonth(Long todoMonth) {
-            this.todoMonth = todoMonth;
-            return this;
-        }
-
-        private TodoDtoBuilder todoDay(Long todoDay) {
-            this.todoDay = todoDay;
-            return this;
-        }
-
-        private TodoDtoBuilder dayOfTheWeek(String dayOfTheWeek) {
-            this.dayOfTheWeek = dayOfTheWeek;
-            return this;
-        }
-
-        private TodoDtoBuilder time(String time) {
-            this.time = time;
-            return this;
-        }
-
-        private TodoDto build() {
-            return new TodoDto(todoId, memberId, categoryId, categoryColor, todoBullet, todoContent, todoYear, todoMonth, todoDay, dayOfTheWeek, time);
-        }
+    public static TodoDto toTodoDto(Long memberId, Long categoryId, Long todoYear, Long todoMonth, Long todoDay) {
+        return TodoDto.builder()
+                .memberId(memberId)
+                .categoryId(categoryId)
+                .todoYear(todoYear)
+                .todoMonth(todoMonth)
+                .todoDay(todoDay)
+                .build();
     }
 }
