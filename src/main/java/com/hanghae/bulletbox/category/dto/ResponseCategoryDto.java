03c1d2ad5eb66021c9655dca487e9a092ddab3c6
jaekyeong1 <kingjk0157@naver.com>
2023-01-13 12:02:05 +0900

    feat: ResponseCategoryDto 추가

---
+++ src/main/java/com/hanghae/bulletbox/category/dto/ResponseCategoryDto.java
@@ -0,0 +1,25 @@
+package com.hanghae.bulletbox.category.dto;
+
+import com.hanghae.bulletbox.diary.dto.DailyDto;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResponseCategoryDto {
+
+    private List<DailyDto> daily;
+
+
+    public ResponseCategoryDto(List<DailyDto> daily){
+        this.daily = daily;
+    }
+
+    public static ResponseCategoryDto toResponseCategoryDto(List<DailyDto> daily){
+        return new ResponseCategoryDto(daily);
+    }
+}
+
