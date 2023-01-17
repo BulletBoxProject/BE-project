04f95eafeb352fa86c76242ca2bed8ec8fbeda23
minho <minhokim0806@gmail.com>
2023-01-17 20:57:15 +0900

    feat: 할일 작성 페이지 조회 API 구현

--- src/main/java/com/hanghae/bulletbox/diary/controller/DailyController.java
+++ src/main/java/com/hanghae/bulletbox/diary/controller/DailyController.java
@@ -5,7 +5,10 @@ import com.hanghae.bulletbox.common.response.Response;
 import com.hanghae.bulletbox.common.security.UserDetailsImpl;
 import com.hanghae.bulletbox.diary.dto.ResponseDailyDto;
 import com.hanghae.bulletbox.diary.dto.ResponseShowDailyDto;
+import com.hanghae.bulletbox.diary.dto.ResponseShowTodoCreatePageDto;
 import com.hanghae.bulletbox.diary.service.DailyService;
+import com.hanghae.bulletbox.diary.service.DailyTodoService;
+import com.hanghae.bulletbox.member.entity.Member;
 import com.hanghae.bulletbox.todo.dto.TodoDto;
 
 import io.swagger.annotations.ApiResponse;
@@ -15,11 +18,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -30,6 +32,7 @@ import springfox.documentation.annotations.ApiIgnore;
 public class DailyController {
 
     private final DailyService dailyService;
+    private final DailyTodoService dailyTodoService;
 
     @Operation(tags = {"Daily"}, summary = "데일리 로그 조회")
     @ApiResponses(value = {
@@ -37,7 +40,7 @@ public class DailyController {
             @ApiResponse(code = 400, message = "존재하지 않는 사용자입니다.")
     })
     @GetMapping
-    public Response showDailyPage(@ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public Response<ResponseDailyDto> showDailyPage(@ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long memberId = userDetails.getMember().getMemberId();
         ResponseDailyDto responseDailyDto = dailyService.showDailyPage(memberId);
         return Response.success(200, "데일리 페이지 조회를 성공했습니다.", responseDailyDto);
@@ -49,7 +52,7 @@ public class DailyController {
             @ApiResponse(code = 400, message = "존재하지 않는 사용자입니다.")
     })
     @GetMapping("/{todoYear}/{todoMonth}/{todoDay}")
-    public Response showDailyPageChangeDay(@PathVariable Long todoYear,
+    public Response<ResponseShowDailyDto> showDailyPageChangeDay(@PathVariable Long todoYear,
                                            @PathVariable Long todoMonth,
                                            @PathVariable Long todoDay,
                                            @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -66,7 +69,7 @@ public class DailyController {
             @ApiResponse(code = 400, message = "존재하지 않는 사용자입니다.")
     })
     @GetMapping("/{todoYear}/{todoMonth}/{todoDay}/{categoryId}")
-    public Response showDailyByCategory(@PathVariable Long todoYear, @PathVariable Long todoMonth,
+    public Response<ResponseCategoryDto> showDailyByCategory(@PathVariable Long todoYear, @PathVariable Long todoMonth,
                                         @PathVariable Long todoDay, @PathVariable Long categoryId,
                                         @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
@@ -74,5 +77,18 @@ public class DailyController {
         TodoDto todoDto = TodoDto.toTodoDto(memberId, categoryId, todoYear, todoMonth, todoDay);
         ResponseCategoryDto responseCategoryDto = dailyService.showDailyByCategory(todoDto);
         return Response.success(200, "카테고리별 데일리 로그 조회를 성공했습니다.", responseCategoryDto);
+    }
+
+    // 할 일 추가 페이지 조회
+    @GetMapping("/todo")
+    public Response<ResponseShowTodoCreatePageDto> showTodoCreatePage(@RequestParam(value = "year") Long year,
+                                       @RequestParam(value = "month") Long month,
+                                       @RequestParam(value = "day") Long day,
+                                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Member member = userDetails.getMember();
+        ResponseShowTodoCreatePageDto responseShowTodoCreatePageDto = dailyTodoService.showTodoCreatePage(member, year, month, day);
+
+        return Response.success(200, "할 일 추가 페이지 조회를 성공했습니다.", responseShowTodoCreatePageDto);
     }
 }
