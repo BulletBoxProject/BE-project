a0ed617b3b9e3b5cdb6abfbcdcab43d9f81b8eda
minho <minhokim0806@gmail.com>
2023-01-17 22:31:40 +0900

    refactor: MemberDto 빌더 추가

--- src/main/java/com/hanghae/bulletbox/member/dto/MemberDto.java
+++ src/main/java/com/hanghae/bulletbox/member/dto/MemberDto.java
@@ -3,6 +3,7 @@ package com.hanghae.bulletbox.member.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,27 +25,23 @@ public class MemberDto {
     @Schema(description = "비밀번호", type = "String", example = "a!1234567", minLength = 8, maxLength = 25)
     private String password;
 
-    @Schema(description = "프로필 이미지", example = "img_url...", type = "String")
-    private String profileImgUrl;
-
-    public MemberDto(String email, String nickname, String password, String profileImgUrl) {
+    @Builder(access = AccessLevel.PRIVATE)
+    private MemberDto(String email, String nickname, String password) {
         this.email = email;
         this.nickname = nickname;
         this.password = password;
-        this.profileImgUrl = profileImgUrl;
-    }
-
-    public static MemberDto toMemberDto(String email, String nickname, String password, String profileImgUrl) {
-        return new MemberDto(email, nickname, password, null);
     }
 
     public static MemberDto toMemberDto(RequestSignupDto requestSignupDto) {
         String email = requestSignupDto.getEmail();
         String nickname = requestSignupDto.getNickname();
         String password = requestSignupDto.getPassword();
-        String profileImgUrl = requestSignupDto.getProfileImgUrl();
 
-        return new MemberDto(email, nickname, password, null);
+        return MemberDto.builder()
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .build();
 
     }
 
@@ -52,6 +49,9 @@ public class MemberDto {
         String email = requestLoginDto.getEmail();
         String password = requestLoginDto.getPassword();
 
-        return new MemberDto(email, null, password, null);
+        return MemberDto.builder()
+                .email(email)
+                .password(password)
+                .build();
     }
 }
