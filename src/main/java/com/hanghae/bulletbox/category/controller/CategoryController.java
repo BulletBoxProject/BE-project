6fb273b2dfa936618c2eaeff109ae8b49cf071d2
hangj97 <gksruwls@naver.com>
2023-01-17 18:12:40 +0900

    feat: 카테고리 생성 API 메서드 컨트롤러에 추가

--- src/main/java/com/hanghae/bulletbox/category/controller/CategoryController.java
+++ src/main/java/com/hanghae/bulletbox/category/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package com.hanghae.bulletbox.category.controller;
 
 import com.hanghae.bulletbox.category.dto.CategoryDto;
+import com.hanghae.bulletbox.category.dto.RequestCreateCategoryDto;
 import com.hanghae.bulletbox.category.dto.ResponseShowCategoryDto;
-import com.hanghae.bulletbox.category.service.CategoryService;
+import com.hanghae.bulletbox.category.service.CategoryPageService;
 import com.hanghae.bulletbox.common.response.Response;
 import com.hanghae.bulletbox.common.security.UserDetailsImpl;
+import com.hanghae.bulletbox.member.entity.Member;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +30,7 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 public class CategoryController {
 
-    private final CategoryService categoryService;
+    private final CategoryPageService categoryService;
 
     @Operation(tags = {"Category"}, summary = "카테고리 목록 페이지 조회")
     @ApiResponses(value = {
@@ -35,10 +39,30 @@ public class CategoryController {
     })
     @GetMapping
     public Response showCategory(@ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Long memberId = userDetails.getMember().getMemberId();
-        CategoryDto categoryDto = CategoryDto.toCategoryDto(memberId);
+
+        Member member = userDetails.getMember();
+        CategoryDto categoryDto = CategoryDto.toCategoryDto(member);
         ResponseShowCategoryDto responseShowCategoryDto = categoryService.showCategory(categoryDto);
 
         return Response.success(200, "카테고리 목록 조회를 성공했습니다.", responseShowCategoryDto);
+    }
+
+    @Operation(tags = {"Category"}, summary = "카테고리 생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "카테고리 생성을 성공했습니다."),
+            @ApiResponse(responseCode = "400", description = "이미 존재하는 카테고리입니다.")
+    })
+    @PostMapping()
+    public Response createCategory(@RequestBody RequestCreateCategoryDto requestCreateCategoryDto,
+                                   @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Member member = userDetails.getMember();
+        String categoryName = requestCreateCategoryDto.getCategoryName();
+        String categoryColor = requestCreateCategoryDto.getCategoryColor();
+
+        CategoryDto categoryDto = CategoryDto.toCategoryDto(categoryName, categoryColor, member);
+        categoryService.createCategory(categoryDto);
+
+        return Response.success(200, "카테고리 생성을 성공했습니다.", null);
     }
 }
