b47c0fcd81e72311786eb062e7bd8c9b9c204e24
hangj97 <gksruwls@naver.com>
2023-01-17 23:03:01 +0900

    feat: 카테고리 수정 API 컨트롤러 추가

--- src/main/java/com/hanghae/bulletbox/category/controller/CategoryController.java
+++ src/main/java/com/hanghae/bulletbox/category/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.hanghae.bulletbox.category.controller;
 
 import com.hanghae.bulletbox.category.dto.CategoryDto;
 import com.hanghae.bulletbox.category.dto.RequestCreateCategoryDto;
+import com.hanghae.bulletbox.category.dto.RequestUpdateCategoryDto;
 import com.hanghae.bulletbox.category.dto.ResponseShowCategoryDto;
 import com.hanghae.bulletbox.category.service.CategoryPageService;
 import com.hanghae.bulletbox.common.response.Response;
@@ -17,7 +18,9 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +67,25 @@ public class CategoryController {
         categoryService.createCategory(categoryDto);
 
         return Response.success(200, "카테고리 생성을 성공했습니다.", null);
+    }
+
+    @Operation(tags = {"Category"}, summary = "카테고리 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "카테고리 수정을 성공했습니다."),
+            @ApiResponse(responseCode = "400", description = "이미 존재하지 않는 카테고리입니다.")
+    })
+    @PutMapping("/{categoryId}")
+    public Response updateCategory(@PathVariable Long categoryId,
+                                   @RequestBody RequestUpdateCategoryDto requestUpdateCategoryDto,
+                                   @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Member member = userDetails.getMember();
+        String categoryName = requestUpdateCategoryDto.getCategoryName();
+        String categoryColor = requestUpdateCategoryDto.getCategoryColor();
+
+        CategoryDto categoryDto = CategoryDto.toCategoryDto(member, categoryId, categoryName, categoryColor);
+        categoryService.updateCategory(categoryDto);
+
+        return Response.success(200, "카테고리 수정을 성공했습니다.", null);
     }
 }
