67cad6167959497ecbdbcec42d42f3d3eb9e8416
hangj97 <gksruwls@naver.com>
2023-01-17 23:03:36 +0900

    feat: 정적 팩토리 메서드 추가 및 Schema 어노테이션 추가

--- src/main/java/com/hanghae/bulletbox/category/dto/CategoryDto.java
+++ src/main/java/com/hanghae/bulletbox/category/dto/CategoryDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import com.hanghae.bulletbox.member.entity.Member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,15 +13,20 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "CategoryDto", description = "CategoryPageService 범용 DTO")
 public class CategoryDto {
 
+    @Schema(description = "카테고리 ID", example = "1", type = "Long")
     private Long categoryId;
 
+    @Schema(description = "Member 참조", type = "Member")
     @JsonIgnore
     private Member member;
 
+    @Schema(description = "카테고리 이름", example = "직장", type = "String")
     private String categoryName;
 
+    @Schema(description = "카테고리 색상", example = "#828282", type = "String")
     private String categoryColor;
 
     @Builder(access = AccessLevel.PRIVATE)
@@ -56,6 +63,15 @@ public class CategoryDto {
                 .categoryName(categoryName)
                 .categoryColor(categoryColor)
                 .member(member)
+                .build();
+    }
+
+    public static CategoryDto toCategoryDto(Member member, Long categoryId, String categoryName, String categoryColor) {
+        return CategoryDto.builder()
+                .member(member)
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .categoryColor(categoryColor)
                 .build();
     }
 }
