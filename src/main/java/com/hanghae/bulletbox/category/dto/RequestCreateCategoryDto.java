9a109ddfccf4ac4ef8484f847b0821dfb40715e4
hangj97 <gksruwls@naver.com>
2023-01-17 18:14:12 +0900

    feat: 카테고리 생성 API 응답 DTO 생성

---
+++ src/main/java/com/hanghae/bulletbox/category/dto/RequestCreateCategoryDto.java
@@ -0,0 +1,26 @@
+package com.hanghae.bulletbox.category.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "RequestCreateCategoryDto", description = "카테고리 생성 요청 DTO")
+public class RequestCreateCategoryDto {
+
+    @Schema(description = "카테고리 이름", example = "직장", type = "String")
+    private String categoryName;
+
+    @Schema(description = "카테고리 색상", example = "#000000", type = "String")
+    private String categoryColor;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private RequestCreateCategoryDto(String categoryName, String categoryColor) {
+        this.categoryName = categoryName;
+        this.categoryColor = categoryColor;
+    }
+}
