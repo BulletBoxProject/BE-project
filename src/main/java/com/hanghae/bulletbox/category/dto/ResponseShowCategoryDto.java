4fbfc8b42159753ec003ffadb9cb6e15f86e5427
hangj97 <gksruwls@naver.com>
2023-01-17 14:28:36 +0900

    feat: 카테고리 목록 조회 응답 DTO 스웨거 스키마 적용

--- src/main/java/com/hanghae/bulletbox/category/dto/ResponseShowCategoryDto.java
+++ src/main/java/com/hanghae/bulletbox/category/dto/ResponseShowCategoryDto.java
@@ -1,15 +1,18 @@
 package com.hanghae.bulletbox.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Schema(name = "ResponseShowCategoryDto", description = "카테고리 목록 조회 응답 DTO")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResponseShowCategoryDto {
 
+    @Schema(description = "CategoryDto로부터 받은 데이터를 담는 변수")
     private List<CategoryDto> categories;
 
     private ResponseShowCategoryDto(List<CategoryDto> categories) {
