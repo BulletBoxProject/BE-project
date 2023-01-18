acaf7e2fa559e190e1d11b8055a2fc10657d9c6d
hangj97 <gksruwls@naver.com>
2023-01-18 20:24:02 +0900

    feat: 마이 페이지 API 서비스 로직 구현

---
+++ src/main/java/com/hanghae/bulletbox/mypage/service/MyPageService.java
@@ -0,0 +1,21 @@
+package com.hanghae.bulletbox.mypage.service;
+
+import com.hanghae.bulletbox.mypage.dto.MyPageDto;
+import com.hanghae.bulletbox.mypage.dto.ResponseShowMyPageDto;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageService {
+
+    public ResponseShowMyPageDto showMyPage(MyPageDto myPageDto) {
+
+        String email = myPageDto.getEmail();
+        String nickname = myPageDto.getNickname();
+
+        return ResponseShowMyPageDto.toResponseShowMyPageDto(email, nickname);
+    }
+}
