936a1d0ac55e678847ff30f4195bad1f4035b261
minho <minhokim0806@gmail.com>
2023-01-18 23:33:19 +0900

    feat: 할 일 추가 API 구현

--- src/main/java/com/hanghae/bulletbox/diary/service/DailyTodoService.java
+++ src/main/java/com/hanghae/bulletbox/diary/service/DailyTodoService.java
@@ -4,8 +4,14 @@ import com.hanghae.bulletbox.category.dto.CategoryDto;
 import com.hanghae.bulletbox.category.service.CategoryService;
 import com.hanghae.bulletbox.diary.dto.DailyTodoDto;
 import com.hanghae.bulletbox.diary.dto.ResponseShowTodoCreatePageDto;
+import com.hanghae.bulletbox.member.dto.MemberDto;
 import com.hanghae.bulletbox.member.entity.Member;
 
+import com.hanghae.bulletbox.todo.TodoBullet;
+import com.hanghae.bulletbox.todo.dto.TodoDto;
+import com.hanghae.bulletbox.todo.dto.TodoMemoDto;
+import com.hanghae.bulletbox.todo.service.TodoMemoService;
+import com.hanghae.bulletbox.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -19,16 +25,42 @@ public class DailyTodoService {
 
     private final CategoryService categoryService;
 
+    private final TodoService todoService;
+
+    private final TodoMemoService todoMemoService;
+
+
     // 데일리 로그 할 일 추가 페이지 조회
-    @Transactional
+    @Transactional(readOnly = true)
     public ResponseShowTodoCreatePageDto showTodoCreatePage(DailyTodoDto dailyTodoDto) {
-        Member member = dailyTodoDto.getMember();
+        MemberDto memberDto = dailyTodoDto.getMemberDto();
         Long year = dailyTodoDto.getYear();
         Long month = dailyTodoDto.getMonth();
         Long day = dailyTodoDto.getDay();
 
-        List<CategoryDto> categoryDtoList = categoryService.findAllCategory(member);
+        List<CategoryDto> categoryDtoList = categoryService.findAllCategory(memberDto);
 
         return ResponseShowTodoCreatePageDto.toResponseShowTodoCreatePageDto(categoryDtoList, year, month, day);
+    }
+
+    // 데일리 로그 할 일 추가
+    @Transactional
+    public void createTodo(DailyTodoDto dailyTodoDto) {
+
+        // 할 일 저장 후 할 일 id값 받기
+        TodoDto todoDto = TodoDto.toTodoDto(dailyTodoDto);
+
+        todoDto = todoService.saveTodo(todoDto);
+
+        // 메모 저장
+        List<TodoMemoDto> todoMemoDtoList = dailyTodoDto.getMemos();
+        MemberDto memberDto = todoDto.getMemberDto();
+
+        for (TodoMemoDto todoMemoDto : todoMemoDtoList) {
+            todoMemoDto.setTodoDto(todoDto);
+            todoMemoDto.setMemberDto(memberDto);
+
+            todoMemoService.saveTodoMemo(todoMemoDto);
+        }
     }
 }
