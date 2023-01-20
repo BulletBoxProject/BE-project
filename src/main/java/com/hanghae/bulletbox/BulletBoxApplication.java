b9c4a54d133319a0d95c01e57ca3e150362af636
hangj97 <gksruwls@naver.com>
2023-01-20 15:35:18 +0900

    fix: @JsonFormat 어노테이션을 사용해 KST 설정

--- src/main/java/com/hanghae/bulletbox/BulletBoxApplication.java
+++ src/main/java/com/hanghae/bulletbox/BulletBoxApplication.java
@@ -4,18 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-
-import java.util.TimeZone;
-
 @EnableJpaAuditing
 @SpringBootApplication
 public class BulletBoxApplication {
-
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(BulletBoxApplication.class, args);
