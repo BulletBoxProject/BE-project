a7823ec0a1ac0e6b193f9e38dcd6b2defb79c641
minho <minhokim0806@gmail.com>
2023-01-20 18:09:10 +0900

    fix: Todo 저장 전 유효성 검사 로직 구현
    저장 전 해당 유저가 실제로 카테고리 아이디를 가지고 있는지 확인.

--- src/main/java/com/hanghae/bulletbox/todo/repository/TodoRepository.java
+++ src/main/java/com/hanghae/bulletbox/todo/repository/TodoRepository.java
@@ -6,6 +6,7 @@ import com.hanghae.bulletbox.todo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
@@ -14,4 +15,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findAllByMemberAndTodoYearAndTodoMonthAndTodoDay(Member member, Long todoYear, Long todoMonth, Long todoDay);
 
     List<Todo> findAllByMemberAndCategoryIdAndTodoYearAndTodoMonthAndTodoDay(Member member, Long categoryId, Long todoYear, Long todoMonth, Long todoDay);
+
+    Optional<Todo> findByTodoIdAndMember(Long todoId, Member member);
+
 }
--- src/main/java/com/hanghae/bulletbox/todo/service/TodoService.java
+++ src/main/java/com/hanghae/bulletbox/todo/service/TodoService.java
@@ -1,5 +1,7 @@
 package com.hanghae.bulletbox.todo.service;
 
+import com.hanghae.bulletbox.category.repository.CategoryRepository;
+import com.hanghae.bulletbox.member.entity.Member;
 import com.hanghae.bulletbox.todo.dto.TodoDto;
 import com.hanghae.bulletbox.todo.entity.Todo;
 import com.hanghae.bulletbox.todo.repository.TodoRepository;
@@ -9,16 +11,49 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+
+import static com.hanghae.bulletbox.common.exception.ExceptionMessage.NOT_FOUND_CATEGORY_MSG;
+import static com.hanghae.bulletbox.common.exception.ExceptionMessage.NOT_FOUND_MEMBER_MSG;
+
+
 @RequiredArgsConstructor
 @Service
 public class TodoService {
 
     private final TodoRepository todoRepository;
+    private final CategoryRepository categoryRepository;
+
+    // 자신의 카테고리 중 카테고리 id가 있는지 조회
+    @Transactional(readOnly = true)
+    protected void checkMemberHasCategoryId(Long categoryId, Member member){
+        categoryRepository.findByCategoryIdAndMember(categoryId, member)
+                .orElseThrow(() -> new NoSuchElementException(NOT_FOUND_CATEGORY_MSG.getMsg()));
+    }
+
+    // Member가 빈 값인지 확인
+    private void checkMemberIsNotEmpty(Member member){
+        if(member == null){
+            throw new NoSuchElementException(NOT_FOUND_MEMBER_MSG.getMsg());
+        }
+    }
+
+    // Todo의 유효성 검사 (멤버 확인, 입력된 카테고리 번호가 내 카테고리 중에서 있는지 확인)
+    private void checkTodoIsSafe(Todo todo){
+        Member member = todo.getMember();
+        Long categoryId = todo.getCategoryId();
+
+        checkMemberIsNotEmpty(member);
+        checkMemberHasCategoryId(categoryId, member);
+    }
+
 
     // 할 일 생성
     @Transactional
     public TodoDto saveTodo(TodoDto todoDto){
         Todo todo = Todo.toTodo(todoDto);
+
+        checkTodoIsSafe(todo);
 
         todoRepository.save(todo);
 
