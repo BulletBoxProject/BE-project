1dc479c9240106c61556768221c136f3b1df3eb1
minho <minhokim0806@gmail.com>
2023-01-24 18:33:49 +0900

    feat: 페이지 조회에 필요한 DTO 생성

---
+++ src/main/java/com/hanghae/bulletbox/diary/dto/ResponseTodoUpdatePageDto.java
@@ -0,0 +1,25 @@
+package com.hanghae.bulletbox.diary.dto;
+
+import com.hanghae.bulletbox.category.dto.CategoryDto;
+import com.hanghae.bulletbox.todo.dto.TodoDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class ResponseTodoUpdatePageDto {
+
+    private List<CategoryDto> categories;
+
+    private TodoDto todo;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ResponseTodoUpdatePageDto(List<CategoryDto> categories, TodoDto todo) {
+        this.categories = categories;
+        this.todo = todo;
+    }
+}
