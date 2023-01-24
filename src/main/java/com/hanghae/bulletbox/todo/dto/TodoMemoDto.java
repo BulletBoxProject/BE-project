46b670951176c63ce821459a4c8e0c1cef39bbb6
minho <minhokim0806@gmail.com>
2023-01-25 01:13:04 +0900

    feat: todoMemo에서 TodoMemoDto로 변환하는 로직 추가

--- src/main/java/com/hanghae/bulletbox/todo/dto/TodoMemoDto.java
+++ src/main/java/com/hanghae/bulletbox/todo/dto/TodoMemoDto.java
@@ -2,6 +2,10 @@ package com.hanghae.bulletbox.todo.dto;
 
 import com.hanghae.bulletbox.member.dto.MemberDto;
 
+import com.hanghae.bulletbox.member.entity.Member;
+import com.hanghae.bulletbox.todo.entity.Todo;
+import com.hanghae.bulletbox.todo.entity.TodoMemo;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,5 +31,23 @@ public class TodoMemoDto {
         this.todoMemoContent = todoMemoContent;
         this.memberDto = memberDto;
         this.todoDto = todoDto;
+    }
+
+    public static TodoMemoDto toTodoMemoDto(TodoMemo todoMemo) {
+        Long todoMemoId = todoMemo.getTodoMemoId();
+        String todoMemoContent = todoMemo.getTodoMemoContent();
+        Todo todo = todoMemo.getTodo();
+        Member member = todoMemo.getMember();
+
+        TodoDto todoDto = TodoDto.toTodoDto(todo);
+        MemberDto memberDto = MemberDto.toMemberDto(member);
+
+        return TodoMemoDto.builder()
+                .todoMemoId(todoMemoId)
+                .todoMemoContent(todoMemoContent)
+                .todoDto(todoDto)
+                .memberDto(memberDto)
+                .build();
+
     }
 }
