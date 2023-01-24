67e2a30507d9242a865a56dbc37b1edc1ff74c5d
hangj97 <gksruwls@naver.com>
2023-01-24 22:42:15 +0900

    feat: 자주 쓰는 할 일 API의 컨트롤러 생성

---
+++ src/main/java/com/hanghae/bulletbox/favorite/controller/FavoriteController.java
@@ -0,0 +1,37 @@
+package com.hanghae.bulletbox.favorite.controller;
+
+import com.hanghae.bulletbox.common.response.Response;
+import com.hanghae.bulletbox.common.security.UserDetailsImpl;
+import com.hanghae.bulletbox.favorite.dto.FavoritePageDto;
+import com.hanghae.bulletbox.favorite.dto.RequestCreateFavoriteTodoDto;
+import com.hanghae.bulletbox.favorite.dto.ResponseCreateFavoriteTodoDto;
+import com.hanghae.bulletbox.favorite.service.FavoritePageService;
+import com.hanghae.bulletbox.member.dto.MemberDto;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/favorites")
+@RequiredArgsConstructor
+public class FavoriteController {
+
+    private final FavoritePageService favoritePageService;
+
+    @PostMapping
+    public Response<ResponseCreateFavoriteTodoDto> createFavoriteTodo(@RequestBody RequestCreateFavoriteTodoDto requestCreateFavoriteTodoDto,
+                                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        MemberDto memberDto = MemberDto.toMemberDto(userDetails);
+        FavoritePageDto favoritePageDto = FavoritePageDto.toFavoritePageDto(requestCreateFavoriteTodoDto, memberDto);
+
+        ResponseCreateFavoriteTodoDto responseCreateFavoriteTodoDto = favoritePageService.createFavoriteTodo(favoritePageDto);
+
+        return Response.success(201, "자주 쓰는 할 일 생성을 성공하였습니다.", responseCreateFavoriteTodoDto);
+    }
+}
