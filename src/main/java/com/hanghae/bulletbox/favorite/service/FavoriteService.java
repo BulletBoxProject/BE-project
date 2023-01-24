10515b5685e60686180199f1bdde12fa3662cae5
hangj97 <gksruwls@naver.com>
2023-01-24 22:51:02 +0900

    feat: 자주 쓰는 할 일 클래스가 레포지토리에 의존하는 서비스 클래스 생성

---
+++ src/main/java/com/hanghae/bulletbox/favorite/service/FavoriteService.java
@@ -0,0 +1,67 @@
+package com.hanghae.bulletbox.favorite.service;
+
+import com.hanghae.bulletbox.category.repository.CategoryRepository;
+import com.hanghae.bulletbox.favorite.dto.FavoriteDto;
+import com.hanghae.bulletbox.favorite.entity.Favorite;
+import com.hanghae.bulletbox.favorite.repository.FavoriteRepository;
+import com.hanghae.bulletbox.member.entity.Member;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+import static com.hanghae.bulletbox.common.exception.ExceptionMessage.NOT_FOUND_CATEGORY_MSG;
+import static com.hanghae.bulletbox.common.exception.ExceptionMessage.NOT_FOUND_MEMBER_MSG;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    // 사용자(member) 카테고리 유효성 검사
+    @Transactional(readOnly = true)
+    protected void checkMemberHasCategoryId(Long categoryId, Member member){
+        categoryRepository.findByCategoryIdAndMember(categoryId, member)
+                .orElseThrow(() -> new NoSuchElementException(NOT_FOUND_CATEGORY_MSG.getMsg()));
+    }
+
+    // member(사용자) null 체크
+    private void checkMemberIsNotNull(Member member){
+        if(member == null){
+            throw new NoSuchElementException(NOT_FOUND_MEMBER_MSG.getMsg());
+        }
+    }
+
+    // FavoriteTodo 유효성 검사
+    private void checkFavoriteTodoIsSafe(Favorite favorite){
+        Member member = favorite.getMember();
+        Long categoryId = favorite.getCategoryId();
+
+        checkMemberIsNotNull(member);
+
+        if (categoryId == null) {
+            return;
+        }
+
+        checkMemberHasCategoryId(categoryId, member);
+    }
+
+    // 자주 쓰는 할 일 생성
+    @Transactional
+    public FavoriteDto saveFavoriteTodo(FavoriteDto favoriteDto) {
+
+        Favorite favorite = Favorite.toFavorite(favoriteDto);
+
+        checkFavoriteTodoIsSafe(favorite);
+
+        favoriteRepository.save(favorite);
+
+        return FavoriteDto.toFavoriteDto(favorite);
+    }
+}
