1cc23ab9349156dded15f45ccc24995bc58f3479
hangj97 <gksruwls@naver.com>
2023-01-24 22:44:01 +0900

    feat: 자주 쓰는 할 일의 서비스 Dto 생성

---
+++ src/main/java/com/hanghae/bulletbox/favorite/dto/FavoritePageDto.java
@@ -0,0 +1,59 @@
+package com.hanghae.bulletbox.favorite.dto;
+
+import com.hanghae.bulletbox.member.dto.MemberDto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FavoritePageDto {
+
+    private MemberDto memberDto;
+
+    private Long favoriteId;
+
+    private String favoriteContent;
+
+    private List<FavoriteMemoDto> favoriteMemos;
+
+    private Long categoryId;
+
+    private String categoryName;
+
+    private String categoryColor;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private FavoritePageDto(MemberDto memberDto, Long favoriteId, String favoriteContent, List<FavoriteMemoDto> favoriteMemos, Long categoryId, String categoryName, String categoryColor) {
+        this.memberDto = memberDto;
+        this.favoriteId = favoriteId;
+        this.favoriteContent = favoriteContent;
+        this.favoriteMemos = favoriteMemos;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryColor = categoryColor;
+    }
+
+    public static FavoritePageDto toFavoritePageDto(RequestCreateFavoriteTodoDto requestCreateFavoriteTodoDto, MemberDto memberDto) {
+
+        String favoriteContent = requestCreateFavoriteTodoDto.getFavoriteContent();
+        List<FavoriteMemoDto> favoriteMemos = requestCreateFavoriteTodoDto.getFavoriteMemos();
+        Long categoryId = requestCreateFavoriteTodoDto.getCategoryId();
+        String categoryName = requestCreateFavoriteTodoDto.getCategoryName();
+        String categoryColor = requestCreateFavoriteTodoDto.getCategoryColor();
+
+        return FavoritePageDto.builder()
+                .memberDto(memberDto)
+                .favoriteContent(favoriteContent)
+                .favoriteMemos(favoriteMemos)
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .categoryColor(categoryColor)
+                .build();
+
+    }
+}
