3fa4305c51c48164d3033249f199f14e9777bc02
hangj97 <gksruwls@naver.com>
2023-01-25 18:55:58 +0900

    feat: Repository 의존하는 서비스 레이어에 member, favorite 기준으로 자주 쓰는 할 일의 메모 찾는 메서드 추가

--- src/main/java/com/hanghae/bulletbox/favorite/service/FavoriteMemoService.java
+++ src/main/java/com/hanghae/bulletbox/favorite/service/FavoriteMemoService.java
@@ -1,10 +1,12 @@
 package com.hanghae.bulletbox.favorite.service;
 
+import com.hanghae.bulletbox.favorite.dto.FavoriteDto;
 import com.hanghae.bulletbox.favorite.dto.FavoriteMemoDto;
 import com.hanghae.bulletbox.favorite.entity.Favorite;
 import com.hanghae.bulletbox.favorite.entity.FavoriteMemo;
 import com.hanghae.bulletbox.favorite.repository.FavoriteMemoRepository;
 import com.hanghae.bulletbox.favorite.repository.FavoriteRepository;
+import com.hanghae.bulletbox.member.dto.MemberDto;
 import com.hanghae.bulletbox.member.entity.Member;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static com.hanghae.bulletbox.common.exception.ExceptionMessage.FAVORITE_NOT_FOUND_MSG;
@@ -59,5 +63,26 @@ public class FavoriteMemoService {
         favoriteMemoRepository.save(favoriteMemo);
 
         return FavoriteMemoDto.toFavoriteMemoDto(favoriteMemo);
+    }
+
+
+    // member, favorite 기준으로 자주 쓰는 할 일의 메모 찾기
+    @Transactional(readOnly = true)
+    public List<FavoriteMemoDto> findAllByMemberAndFavorite(MemberDto memberDto, FavoriteDto favoriteDto) {
+
+        Member member = Member.toMember(memberDto);
+        Favorite favorite = Favorite.toFavorite(favoriteDto);
+
+        List<FavoriteMemoDto> favoriteMemoDtoList = new ArrayList<>();
+        List<FavoriteMemo> favoriteMemoList = favoriteMemoRepository.findAllByMemberAndFavorite(member, favorite);
+
+        for (FavoriteMemo favoriteMemo : favoriteMemoList) {
+
+            // Entity -> Dto 변환
+            FavoriteMemoDto favoriteMemoDto = FavoriteMemoDto.toFavoriteMemoDto(favoriteMemo);
+            favoriteMemoDtoList.add(favoriteMemoDto);
+        }
+
+        return favoriteMemoDtoList;
     }
 }
