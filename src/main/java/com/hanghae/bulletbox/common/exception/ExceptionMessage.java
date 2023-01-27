39d22e8204ea19e7788fe51091ad55a9e7df8a4e
jaekyeong1 <kingjk0157@naver.com>
2023-01-27 21:45:28 +0900

    refactor : 예외처리 수정

--- src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
+++ src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionMessage {
-    EXCEPTION_MESSAGE_SAMPLE(401, "예외 내용을 적어주세요"),
 
     NOT_FOUND_MEMBER_MSG(404, "존재하지 않는 사용자입니다."),
 
@@ -25,7 +24,7 @@ public enum ExceptionMessage {
 
     SOCIAL_LOGIN_ERROR(401, "소셜 로그인에 실패하였습니다."),
 
-    NOT_MATCH_REFRESHTOKEN(401, "리프레쉬 토큰이 일치하지 않습니다."),
+    NOT_MATCH_REFRESH_TOKEN(401, "리프레쉬 토큰이 일치하지 않습니다."),
 
     // Todo관련
     BULLET_NOT_FOUND_MSG(404, "불렛이 존재하지 않습니다."),
