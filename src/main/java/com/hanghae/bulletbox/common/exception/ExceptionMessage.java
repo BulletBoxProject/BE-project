9144506d20645ad0fa6c6cf1bd96e812ff0daad0
jaekyeong1 <kingjk0157@naver.com>
2023-01-27 16:32:18 +0900

    feat : RefreshToken 예외 처리

--- src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
+++ src/main/java/com/hanghae/bulletbox/common/exception/ExceptionMessage.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionMessage {
-    EXCEPTION_MESSAGE_SAMPLE(401,"예외 내용을 적어주세요"),
+    EXCEPTION_MESSAGE_SAMPLE(401, "예외 내용을 적어주세요"),
 
     NOT_FOUND_MEMBER_MSG(404, "존재하지 않는 사용자입니다."),
 
     DUPLICATE_EMAIL_MSG(400, "이미 가입한 이메일입니다."),
 
-    NOT_FOUND_EMAIL_MSG(400,"이메일 또는 비밀번호를 확인해주세요."),
+    NOT_FOUND_EMAIL_MSG(400, "이메일 또는 비밀번호를 확인해주세요."),
 
     DIFFERENT_PASSWORD_MSG(400, "이메일 또는 비밀번호가 일치하지 않습니다."),
 
@@ -19,16 +19,20 @@ public enum ExceptionMessage {
     FAILED_TO_SEND_MAIL(400, "메일 발송에 실패하였습니다."),
 
     /*Jwt, Security */
-    TOKEN_NOT_FOUND_MSG(401,"토큰이 존재하지 않습니다."),
+    TOKEN_NOT_FOUND_MSG(401, "토큰이 존재하지 않습니다."),
 
-    INVALID_TOKEN_MSG(401,"토큰이 유효하지 않습니다."),
+    INVALID_TOKEN_MSG(401, "토큰이 유효하지 않습니다."),
+
+    SOCIAL_LOGIN_ERROR(401, "소셜 로그인에 실패하였습니다."),
+
+    NOT_MATCH_REFRESHTOKEN(401, "리프레쉬 토큰이 일치하지 않습니다."),
 
     // Todo관련
     BULLET_NOT_FOUND_MSG(404, "불렛이 존재하지 않습니다."),
 
     TODO_NOT_FOUND_MSG(404, "할 일이 존재하지 않습니다."),
 
-   // Category
+    // Category
     DUPLICATE_CATEGORYNAME_MSG(400, "이미 존재하는 카테고리입니다."),
 
     NOT_FOUND_CATEGORY_MSG(400, "존재하지 않는 카테고리입니다.");
