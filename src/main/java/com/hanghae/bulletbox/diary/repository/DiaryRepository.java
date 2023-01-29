bdcb3709c304dfb38a76072b37d4cc04e0839573
minho <minhokim0806@gmail.com>
2023-01-30 02:37:34 +0900

    feat: 일기장 조회 날짜 변경 API 구현

--- src/main/java/com/hanghae/bulletbox/diary/repository/DiaryRepository.java
+++ src/main/java/com/hanghae/bulletbox/diary/repository/DiaryRepository.java
@@ -6,7 +6,11 @@ import com.hanghae.bulletbox.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findAllByMemberAndYearAndMonth(Member member, Long year, Long month);
+
+    Optional<Diary> findByMemberAndYearAndMonthAndDay(Member member, Long year, Long month, Long day);
+
 }
