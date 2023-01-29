fd071a7c4f59d0f39adc7c4dad12394bd957af36
hangj97 <gksruwls@naver.com>
2023-01-30 02:46:00 +0900

    fix: 배포 스크립트 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,7 +16,16 @@ echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp ./build/libs/*.jar $PROJECT_ROOT/
 
 # jar 파일 실행
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+
 echo nohup java -jar $JAR_FILE > $APP_LOG > $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
