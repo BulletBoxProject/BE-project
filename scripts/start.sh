6f240cc7498557beb7be04b666593b53a9c4a8d3
hangj97 <gksruwls@naver.com>
2023-01-30 03:19:37 +0900

    fix: 배포 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,19 +3,16 @@
 PROJECT_ROOT="/home/ubuntu/BulletBox"
 JAR_FILE="bulletBox-webapp.jar"
 
-cd $PROJECT_ROOT
-
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp ./build/libs/*.jar $PROJECT_ROOT/
+# 현재 구동중인 어플리케이션 PID 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
 
-# jar 파일 실행
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
@@ -24,9 +21,12 @@ else
   sleep 5
 fi
 
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT/jar
 
-echo nohup java -jar $JAR_FILE > $APP_LOG > $ERROR_LOG &
+JAR_NAME=$(ls $PROJECT_ROOT | grep 'bulletBox' | tail -n 1)
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $PROJECT_ROOT/jar/$JAR_NAME > $APP_LOG > $ERROR_LOG &
