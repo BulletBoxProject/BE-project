64d027855346506743841b2c63d814ea81f8e900
minho <minhokim0806@gmail.com>
2023-01-30 00:47:24 +0900

    feat: Diary 엔티티를 대체할 수 있는 DiaryDto 생성

---
+++ src/main/java/com/hanghae/bulletbox/diary/dto/DiaryDto.java
@@ -0,0 +1,67 @@
+package com.hanghae.bulletbox.diary.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import com.hanghae.bulletbox.diary.entity.Diary;
+import com.hanghae.bulletbox.member.dto.MemberDto;
+import com.hanghae.bulletbox.member.entity.Member;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DiaryDto {
+
+    private Long diaryId;
+
+    @JsonIgnore
+    private MemberDto memberDto;
+
+    private String diaryContent;
+
+    private String emotion;
+
+    private Long year;
+
+    private Long month;
+
+    private Long day;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private DiaryDto(Long diaryId, MemberDto memberDto, String diaryContent, String emotion, Long year, Long month, Long day) {
+        this.diaryId = diaryId;
+        this.memberDto = memberDto;
+        this.diaryContent = diaryContent;
+        this.emotion = emotion;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    // Diary를 DiaryDto로 변환
+    public static DiaryDto toDiaryDto(Diary diary) {
+        Long diaryId = diary.getDiaryId();
+        Member member = diary.getMember();
+        MemberDto memberDto = MemberDto.toMemberDto(member);
+        String diaryContent = diary.getDiaryContent();
+        String emotion = diary.getEmotion();
+        Long year = diary.getYear();
+        Long month = diary.getMonth();
+        Long day = diary.getDay();
+
+        return DiaryDto.builder()
+                .diaryId(diaryId)
+                .memberDto(memberDto)
+                .diaryContent(diaryContent)
+                .emotion(emotion)
+                .year(year)
+                .month(month)
+                .day(day)
+                .build();
+    }
+}
