7df7eaac774f05328568e23b7e69f2f9c0359d83
hangj97 <gksruwls@naver.com>
2023-02-07 01:33:57 +0900

    fix: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 DOCKER_NAME=$(docker ps -qa)
-echo "TIME_NOW > 현재 구동중인 컨테이너는 $DOCKER_NAME 입니다." >> $DEPLOY_LOG
-
-sudo docker pull gksrywls97/bulletbox:web
-sudo docker pull gksrywls97/bulletbox:redis
+echo "$TIME_NOW > 현재 구동중인 컨테이너는 $DOCKER_NAME 입니다." >> $DEPLOY_LOG
 docker-compose down
+
+docker pull gksrywls97/bulletbox:web
+docker pull gksrywls97/bulletbox:redis
+
 docker-compose up -d --remove-orphans
 docker image prune -f
 
