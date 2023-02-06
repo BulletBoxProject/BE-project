4c052fbdf76f6b7170c38d4100eaada17001103c
hangj97 <gksruwls@naver.com>
2023-02-07 02:18:24 +0900

    fix: ci-cd 코드 원상 복구

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@ TIME_NOW=$(date +%c)
 
 DOCKER_NAME=$(docker ps -qa)
 echo "$TIME_NOW > 현재 구동중인 컨테이너는 $DOCKER_NAME 입니다." >> $DEPLOY_LOG
-docker-compose down
-
-docker pull gksrywls97/bulletbox:web
-docker pull gksrywls97/bulletbox:redis
-
-docker-compose up -d --remove-orphans
-docker image prune -f
+#docker-compose down
+#
+#docker pull gksrywls97/bulletbox:web
+#docker pull gksrywls97/bulletbox:redis
+#
+#docker-compose up -d --remove-orphans
+#docker image prune -f
 
